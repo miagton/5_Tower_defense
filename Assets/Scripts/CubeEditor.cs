2133140c03294941967ce0f67510b643c72165b1
Miagkostup_Igor <justbl0w1991@gmail.com>
2020-06-21 11:11:41 +0300

    labeled + coroutine path moving added

---
+++ Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
+{   
+    [SerializeField] [Range(1f, 20f)] float gridSize=10f;
+
+    TextMesh textMesh;
+
+    private void Awake()
+    {
+        textMesh = GetComponentInChildren<TextMesh>();
+        
+    }
+
+    void Update()
+    {
+        Vector3 snapPosition;
+      
+        snapPosition.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;//Rounds inner math then multipl by 10 to get world coordinate back
+        snapPosition.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+        transform.position = new Vector3(snapPosition.x, 0, snapPosition.z);//new Vector3(snapPosition.x, 0, snapPosition.z);
+
+        string labelText = snapPosition.x / gridSize + "," + snapPosition.z / gridSize;//label  to get not exact coordinates but proper label
+        textMesh.text = labelText;
+        gameObject.name = labelText;
+    }
+
+   
+}
---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<WayPoint> path;
+
+    [Tooltip("Time between each move")]
+    [SerializeField] float dwellingTime = 1f; 
+
+    void Start()
+    {
+        StartCoroutine(FollowPath());
+    }
+
+   
+
+    IEnumerator FollowPath()
+    {
+            print("Starting patrol!");
+       
+        foreach (var block in path)
+        {
+            transform.position = block.transform.position;
+            print("visiting block : " + block.name);
+            yield return new WaitForSeconds(dwellingTime);
+        }
+            print("Walking finished!");
+    }
+}
