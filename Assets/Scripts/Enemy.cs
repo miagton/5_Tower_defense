e8eaa74bd45a6609d792301dfd8cd05f7be2c3c3
Miagkostup_Igor <justbl0w1991@gmail.com>
2020-06-24 22:41:53 +0300

    -Turrets checking distance to enemies -turrets shooting -enemies dying -vfx added

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+   
+
+    [Tooltip("Time between each move")]
+    [SerializeField] float dwellingTime = 1f;
+
+    [SerializeField] GameObject hitEffect = null;
+    [SerializeField] GameObject deathEffect = null;
+    [SerializeField] int hitsToDie = 6;
+    [SerializeField] Transform parent;
+    
+    Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+        PathFinder pathFinder = FindObjectOfType<PathFinder>();
+        var path = pathFinder.GetPath();
+         StartCoroutine(FollowPath(path));
+    }
+
+   
+
+    IEnumerator FollowPath(List<WayPoint> path)
+    {
+            print("Starting patrol!");
+       
+        foreach (WayPoint block in path)
+        {
+            transform.position = block.transform.position;
+            
+            yield return new WaitForSeconds(dwellingTime);
+        }
+            print("Walking finished!");
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if (hitsToDie <= 0)
+        {
+            ProccesDeath();
+        }
+    }
+
+    private void ProcessHit()
+    {
+        if (hitEffect != null)
+        {
+            GameObject fx = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            fx.transform.parent = parent;
+        }
+        hitsToDie--;
+        print(hitsToDie);
+
+    }
+    private void ProccesDeath()
+    {
+        if (deathEffect != null)
+        {
+            GameObject fx = Instantiate(deathEffect, transform.position, Quaternion.identity);
+            fx.transform.parent = parent;
+        }
+        Destroy(gameObject);
+    }
+
+   
+}
---
+++ Assets/Scripts/TowerController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerController : MonoBehaviour
+{
+    [SerializeField] Transform objectToSpin;
+    [SerializeField] Transform objToLookAt;
+    [SerializeField] GameObject gun;
+
+    void Update()
+    {
+        
+        AttackEnemy();
+    }
+
+    private void AttackEnemy()
+    {
+        if (objToLookAt == null)
+        {
+            EnableEmission(false);
+            return;
+        }
+        if(Vector3.Distance(transform.position,objToLookAt.position)<=30f)
+        {
+            objectToSpin.LookAt(objToLookAt);
+            EnableEmission(true);
+        }
+        else 
+        {
+            EnableEmission(false);
+        }
+    }
+
+    private void EnableEmission(bool IsActive)
+    {
+       
+        var gunEmmision = gun.GetComponent<ParticleSystem>().emission;
+        gunEmmision.enabled = IsActive;
+    }
+}
