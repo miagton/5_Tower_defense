7d597a4fd5c3dbdee94f4619c30110240af3e29f
Miagkostup_Igor <justbl0w1991@gmail.com>
2020-06-27 09:42:01 +0300

    tower factory working

---
+++ Assets/Scripts/TowerFactory.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerFactory : MonoBehaviour
+{
+    [SerializeField] TowerController[] towers = null;
+    [SerializeField] int towerLimit = 3;
+
+    int towersCreated = 0;
+   
+    public void AddTower(TowerHolder baseHolder)
+    {
+        if (towersCreated < towerLimit)
+        {
+            CreateNewTower(baseHolder);
+
+           
+
+        }
+        else 
+        {
+            MoveExistingTower();
+        }
+    }
+
+    private static void MoveExistingTower()
+    {
+        print("cant place more towers");
+    }
+
+    private void CreateNewTower(TowerHolder baseHolder)
+    {
+        towersCreated++;
+        int i = Random.Range(0, towers.Length);
+        TowerController newTower = Instantiate(towers[i], baseHolder.transform.position, Quaternion.identity);
+        newTower.transform.parent = this.transform;
+        baseHolder.isOccupiedByTower = true;
+    }
+}
--- Assets/Scripts/TowerHolder.cs
+++ Assets/Scripts/TowerHolder.cs
@@ -4,24 +4,18 @@ using UnityEngine;
 
 public class TowerHolder : MonoBehaviour
 {
-    [SerializeField] TowerController[] towers = null;
-    Transform parentForTowers = null;
-    bool isOccupiedByTower = false;
+   
+   public bool isOccupiedByTower = false;
 
-    void Start()
-    {
-        parentForTowers = GameObject.Find("Towers").transform;
-    }
+   
 
     private void OnMouseDown()
     {
       
         if ( !isOccupiedByTower)
         {
-            int i = Random.Range(0, towers.Length );
-            TowerController newTower = Instantiate(towers[i], transform.position, Quaternion.identity);
-            newTower.transform.parent = parentForTowers;
-            isOccupiedByTower = true;
+            FindObjectOfType<TowerFactory>().AddTower(this);
+            
         }
        
         Debug.Log("Placing Tower at: " + gameObject.name);
